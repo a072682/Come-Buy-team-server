6c2b5c6f4a21af4f5c546169ecd88f2bb5357c8b
CubeAndy <a072682@gmail.com>
2025-10-13 22:23:37 +0800

    小修改

--- db/db.js
+++ db/db.js
@@ -3,11 +3,19 @@ const { Pool } = require('pg'); // 引入 pg 套件，並從中取出 Pool（連
 require('dotenv').config(); // 引入 dotenv 套件，讓我們可以讀取 .env 檔案中的環境變數
 
 const pool = new Pool({ // 建立一個新的 Pool 實例，裡面傳入連線設定
-user: process.env.DB_USER,
-host: process.env.DB_HOST,
-database: process.env.DB_DATABASE,
-password: process.env.DB_PASSWORD,
-port: process.env.DB_PORT,
+//此區塊為開發時內容
+    // user: process.env.DB_USER,
+    // host: process.env.DB_HOST,
+    // database: process.env.DB_DATABASE,
+    // password: process.env.DB_PASSWORD,
+    // port: process.env.DB_PORT,
+//此區塊為開發時內容
+
+//上傳至render時請更改內容
+    connectionString: process.env.DATABASE_URL,
+    // 外部連線一定要 SSL；多數情況要關閉嚴格驗證
+    ssl: { rejectUnauthorized: false },
+//上傳至render時請更改內容
 });
 
 module.exports = pool; // 將這個 pool 導出（export），讓其他檔案可以使用
